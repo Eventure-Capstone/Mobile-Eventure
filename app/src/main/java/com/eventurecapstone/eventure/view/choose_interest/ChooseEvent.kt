d5f01b5d9a21d142bc32e28122e749ed37fd79c6
Iksanard <iksanardika@gmail.com>
2024-06-14 15:07:25 +0700

    create activity initial file

---
+++ app/src/main/java/com/eventurecapstone/eventure/view/choose_interest/ChooseEvent.kt
@@ -0,0 +1,21 @@
+package com.eventurecapstone.eventure.view.choose_interest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.eventurecapstone.eventure.R
+
+class ChooseEvent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_choose_event)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
