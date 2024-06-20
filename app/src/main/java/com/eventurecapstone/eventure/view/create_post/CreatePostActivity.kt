382dd391e2383bf7706316e0bc95c2f059465538
Imamsubekti26 <imam0subekti@gmail.com>
2024-06-20 09:20:35 +0700

    view for create post done

--- app/src/main/java/com/eventurecapstone/eventure/view/create_post/CreatePostActivity.kt
+++ app/src/main/java/com/eventurecapstone/eventure/view/create_post/CreatePostActivity.kt
@@ -1,21 +1,136 @@
 package com.eventurecapstone.eventure.view.create_post
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.eventurecapstone.eventure.R
+import androidx.lifecycle.ViewModelProvider
+import com.eventurecapstone.eventure.databinding.ActivityCreatePostBinding
+import com.eventurecapstone.eventure.di.ViewModelFactory
+import java.util.Calendar
 
 class CreatePostActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreatePostBinding
+    private lateinit var model: CreatePostViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_create_post)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityCreatePostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(
+            this,
+            ViewModelFactory.getInstance(this)
+        )[CreatePostViewModel::class.java]
+
+        setupActionBar()
+        setupImagePreview()
+        setupInputNonText()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
+            if (uri != null) {
+                model.setCurrentImageUri(uri)
+            } else {
+                Log.d("Photo Picker", "No media selected")
+            }
+        }
+
+    private fun setupImagePreview(){
+        model.currentImageUri.observe(this){
+            Log.d("Image URI", "showImage: $it")
+            binding.ivCreatePost.setImageURI(it)
         }
     }
+
+    private fun setupCategoryDropDown(){
+        model.category.observe(this){
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, it)
+            binding.categoryDropDown.setAdapter(adapter)
+        }
+    }
+
+    private fun setupInputNonText(){
+        setupCategoryDropDown()
+        binding.btnUpload.setOnClickListener { startGallery() }
+        binding.eventStartDateEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.eventStartDateEditText.clearFocus()
+                showDatePicker(binding.eventStartDateEditText)
+            }
+        }
+        binding.eventEndDateEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.eventEndDateEditText.clearFocus()
+                showDatePicker(binding.eventEndDateEditText)
+            }
+        }
+        binding.eventStartTimeEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.eventStartTimeEditText.clearFocus()
+                showTimePicker(binding.eventStartTimeEditText)
+            }
+        }
+        binding.eventEndTimeEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.eventEndTimeEditText.clearFocus()
+                showTimePicker(binding.eventEndTimeEditText)
+            }
+        }
+    }
+
+    private fun showDatePicker(editText: EditText) {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val date = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                editText.setText(date)
+            }, year, month, day
+        )
+        datePickerDialog.show()
+    }
+
+    @SuppressLint("DefaultLocale", "SetTextI18n")
+    private fun showTimePicker(editText: EditText) {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val timePickerDialog = TimePickerDialog(
+            this,
+            { _, selectedHour, selectedMinute ->
+                val time = String.format("%02d:%02d", selectedHour, selectedMinute)
+                editText.setText(time)
+            }, hour, minute, true
+        )
+        timePickerDialog.show()
+    }
+
 }
--- app/src/main/java/com/eventurecapstone/eventure/view/create_post/CreatePostViewModel.kt
+++ app/src/main/java/com/eventurecapstone/eventure/view/create_post/CreatePostViewModel.kt
@@ -1,8 +1,31 @@
 package com.eventurecapstone.eventure.view.create_post
 
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eventurecapstone.eventure.data.entity.Event
+import com.eventurecapstone.eventure.data.repository.EventRepository
 import com.eventurecapstone.eventure.data.repository.PreferenceRepository
+import kotlinx.coroutines.launch
 
-class CreatePostViewModel(private val preferenceRepository: PreferenceRepository): ViewModel() {
+class CreatePostViewModel(
+    private val eventRepository: EventRepository
+): ViewModel() {
+
+    private val _currentImageUri = MutableLiveData<Uri>()
+    val currentImageUri: LiveData<Uri> get() = _currentImageUri
+    fun setCurrentImageUri(uri: Uri) = _currentImageUri.postValue(uri)
+
+    private val _categories = MutableLiveData<List<String>>()
+    val category: LiveData<List<String>> get() {
+        viewModelScope.launch {
+            val res = eventRepository.getCategory()
+            val value = res?.data?.filterNotNull() ?: listOf()
+            _categories.postValue(value)
+        }
+        return _categories
+    }
 
 }
